8dcb0fd6aa44bc075a25e2551cc9951e45be9aa9
Chang Liu <24112359@student.uwa.edu.au>
2025-04-25 13:04:26 +0800

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 import os
 
 # Basic configuration for Flask + SQLAlchemy
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key-of-the-project'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
