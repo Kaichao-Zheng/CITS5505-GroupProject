29953097cf9a583d18abac2a23b0da322633905b
Chang Liu <24112359@student.uwa.edu.au>
2025-04-25 12:12:24 +0800

    Add database routes to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from app.db_models import db
+from app.routes.api_routes import api_bp
 
 app = Flask(__name__)
+app.config.from_pyfile('../instance/config.py')
+db.init_app(app)
+app.register_blueprint(api_bp, url_prefix='/api')
 
 from app import routes
